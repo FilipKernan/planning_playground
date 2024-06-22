0c059fe9fa153b0b1726cfe44798447716558b97
Filip Kernan <filip.kernan@gmail.com>
2024-06-22 08:46:00 -0600

    adding timing

--- test_planner.py
+++ test_planner.py
@@ -10,15 +10,16 @@ import numpy as np
 if __name__ == '__main__':
     # Create a map
     print("creating map")
-    map = import_map.Map2d("planning_playground/map/map_dense84.png", 250)
+    map = import_map.Map2d("planning_playground/map/map991.png", 100)
     print("created map")
     # todo: make a ui for chousing the start and goal
     start = (0, 0, 0)
-    goal = (237, 237, 0)
+    goal = (95, 95, 0)
     motion_model = holonomic_model.HolonomicModel([1, 1], 1)
     a_star = a_star_planner.AStarPlanner(map, motion_model)
-    path = a_star.plan(start, goal)
+    path, delta_time = a_star.plan(start, goal)
     print(path)
     viz = viz.VizPlan(map, path, motion_model, start, goal)
     # viz.plot_map()
     viz.plot_path()
+    print("delta time", delta_time)
